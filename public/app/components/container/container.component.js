06374f63add2f2a1e6615cf249aea76ae9f4b4f4
John Gainfort <john.gainfort@realeyes.com>
2015-08-07 09:42:11 -0600

    Fixed issue where content tags were not working

--- public/app/components/container/container.component.js
+++ public/app/components/container/container.component.js
@@ -10,38 +10,19 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 var angular2_1 = require('angular2/angular2');
-var champions_component_1 = require('app/components/champions/champions.component');
-var items_component_1 = require('app/components/items/items.component');
 var ContainerComponent = (function () {
-    function ContainerComponent(content, dynamicComponentLoader, elementRef) {
-        var child;
-        switch (content) {
-            case 'champions':
-                child = champions_component_1.ChampionsComponent;
-                break;
-            case 'items':
-                child = items_component_1.ItemsComponent;
-                break;
-            default:
-                console.error('Invalid content type.');
-        }
-        dynamicComponentLoader.loadIntoLocation(child, elementRef, 'content');
+    function ContainerComponent() {
     }
     ContainerComponent = __decorate([
         angular2_1.Component({
             selector: 'container',
-            properties: ['title', 'content']
+            properties: ['title']
         }),
         angular2_1.View({
-            templateUrl: 'app/components/container/container.html',
-            directives: [champions_component_1.ChampionsComponent, items_component_1.ItemsComponent]
-        }),
-        __param(0, angular2_1.Attribute('content')), 
-        __metadata('design:paramtypes', [String, angular2_1.DynamicComponentLoader, angular2_1.ElementRef])
+            templateUrl: 'app/components/container/container.html'
+        }), 
+        __metadata('design:paramtypes', [])
     ], ContainerComponent);
     return ContainerComponent;
 })();
